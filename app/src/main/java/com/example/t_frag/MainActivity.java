e119b99b09ff1bfbe47d17801983aca8dc938a37
Deepanc7 <deepanc2001@gmail.com>
2022-06-02 22:39:21 +0530

    new code

--- app/src/main/java/com/example/t_frag/MainActivity.java
+++ app/src/main/java/com/example/t_frag/MainActivity.java
@@ -1,80 +1,30 @@
 package com.example.t_frag;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
 
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button next_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabLayout tabLayout = findViewById(R.id._tab);
-        ViewPager2 viewPager = findViewById(R.id.viewpager);
-
-        tabLayout.addTab(tabLayout.newTab().setText("Frag 1"), 0);
-        tabLayout.addTab(tabLayout.newTab().setText("Frag 2"), 1);
-        tabLayout.addTab(tabLayout.newTab().setText("Frag 3"), 2);
-
-        FragmentStateAdapter pagerAdapter = new ScreenSlidePagerAdapter(MainActivity.this);
-        viewPager.setAdapter(pagerAdapter);
-
-        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                if (position==0) tab.setText("Frag 1");
-                if (position==1) tab.setText("Frag 2");
-                if (position==2) tab.setText("Frag 3");
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
             }
-        }).attach();
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });
-
-        viewPager.setCurrentItem(0);
-
-    }
-
-
-    class ScreenSlidePagerAdapter extends FragmentStateAdapter {
-        public ScreenSlidePagerAdapter(AppCompatActivity fa) {
-            super(fa);
-        }
-
-        @Override
-        public Fragment createFragment(int position) {
-            Fragment frag_new = null;
-            if (position==0) frag_new= new fragment1();
-            if (position==1) frag_new= new fragment2();
-            if (position==2) frag_new= new fragment3();
-
-            return frag_new;
-        }
-
-        @Override
-        public int getItemCount() {
-            return 3;
-        }
+        },2000);
     }
 
 }
