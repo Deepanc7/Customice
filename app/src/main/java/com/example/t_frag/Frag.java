2acd8d4b2fc16e7ae27be5e6da4d0bae69f914d9
Deepanc7 <deepanc2001@gmail.com>
2022-06-02 22:43:52 +0530

    new code

---
+++ app/src/main/java/com/example/t_frag/Frag.java
@@ -0,0 +1,80 @@
+package com.example.t_frag;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class Frag extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frag);
+
+        TabLayout tabLayout = findViewById(R.id._tab);
+        ViewPager2 viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Frag 1"), 0);
+        tabLayout.addTab(tabLayout.newTab().setText("Frag 2"), 1);
+        tabLayout.addTab(tabLayout.newTab().setText("Frag 3"), 2);
+
+        FragmentStateAdapter pagerAdapter = new ScreenSlidePagerAdapter(Frag.this);
+        viewPager.setAdapter(pagerAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                if (position==0) tab.setText("Frag 1");
+                if (position==1) tab.setText("Frag 2");
+                if (position==2) tab.setText("Frag 3");
+            }
+        }).attach();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+        viewPager.setCurrentItem(0);
+
+    }
+
+
+    class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+        public ScreenSlidePagerAdapter(AppCompatActivity fa) {
+            super(fa);
+        }
+
+        @Override
+        public Fragment createFragment(int position) {
+            Fragment frag_new = null;
+            if (position==0) frag_new= new fragment1();
+            if (position==1) frag_new= new fragment2();
+            if (position==2) frag_new= new fragment3();
+
+            return frag_new;
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/t_frag/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.t_frag;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    ImageView imageView;
+    Button next_btn;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        next_btn = (Button) findViewById(R.id.next_button);
+
+        next_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),Frag.class);
+                startActivity(intent);
+            }
+        });
+        }
+    }
