6e33bb35f42898d538ce5ac10dcd26aedf4a48c7
Deepanc7 <deepanc2001@gmail.com>
2022-06-03 23:23:29 +0530

    fragment 1

--- app/src/main/java/com/example/t_frag/fragment1.java
+++ app/src/main/java/com/example/t_frag/fragment1.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,23 +16,46 @@ public class fragment1 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View fragview = inflater.inflate(R.layout.fragment_fragment1, container, false);
-        return fragview;
+        View view = inflater.inflate(R.layout.fragment_fragment1, container, false);
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ImageButton simpleImageButtonCup = (ImageButton)view.findViewById(R.id.imageButton2);
+        ImageButton simpleImageButtonCone = (ImageButton)view.findViewById(R.id.imageButton3);
+        ImageButton simpleImageButtonCandy = (ImageButton)view.findViewById(R.id.imageButton);
+        ImageButton simpleImageButtonParfait = (ImageButton)view.findViewById(R.id.imageButton4);
 
-        Button button1 = view.findViewById(R.id.button1);
-        TextView textview1 = view.findViewById(R.id.textView1);
+        TextView textview4 = view.findViewById(R.id.textView4);
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        simpleImageButtonCup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textview1.setText("Type");
+                textview4.setText("Cup");
             }
         });
+        simpleImageButtonCone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textview4.setText("Cone");
+            }
+        });
+        simpleImageButtonCandy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textview4.setText("Candy");
+            }
+        });
+        simpleImageButtonParfait.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textview4.setText("Parfait");
+            }
+        });
+
+
 
     }
 }
