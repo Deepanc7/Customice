4fa708e29b12f112f10101f1940cec584dd81eb5
Deepanc7 <deepanc2001@gmail.com>
2022-06-06 21:23:20 +0530

    qr code Scanner

--- app/src/main/java/com/example/t_frag/MainActivity.java
+++ app/src/main/java/com/example/t_frag/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                Intent intent = new Intent(MainActivity.this,MainActivity3.class);
                 startActivity(intent);
                 finish();
             }
--- app/src/main/java/com/example/t_frag/MainActivity2.java
+++ app/src/main/java/com/example/t_frag/MainActivity2.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
     ImageView imageView;
     Button next_btn;
+    String data;
+    TextView rcv;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
@@ -24,5 +28,12 @@ public class MainActivity2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        }
+        //data=MainActivity3.getActivityInstance().getData();
+
+        //rcv = (TextView) findViewById(R.id.textView5);
+        //Intent intent = getIntent();
+        //String str = intent.getStringExtra("data");
+        //rcv.setText("Welcome to ", TextView.BufferType.valueOf(str));
+
+    }
     }
